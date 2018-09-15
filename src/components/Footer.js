18e7ee7837acdd059da5e9a9e6294cb7b6146691
zach797a <zachhardesty7@gmail.com>
2018-09-15 18:15:24 -0500

    add footer template

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import {
+  Container,
+  Grid,
+  Segment,
+  Header,
+  List
+} from 'semantic-ui-react'
+
+import './Footer.scss'
+
+const Footer = () => (
+  <>
+    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='About' />
+              <List link inverted>
+                <List.Item as='a'>Sitemap</List.Item>
+                <List.Item as='a'>Contact Us</List.Item>
+                <List.Item as='a'>Religious Ceremonies</List.Item>
+                <List.Item as='a'>Gazebo Plans</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Services' />
+              <List link inverted>
+                <List.Item as='a'>Banana Pre-Order</List.Item>
+                <List.Item as='a'>DNA FAQ</List.Item>
+                <List.Item as='a'>How To Access</List.Item>
+                <List.Item as='a'>Favorite X-Men</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header as='h4' inverted>
+                Footer Header
+              </Header>
+              <p>
+                Extra space for a call to action inside the footer that could help re-engage users.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
+    <Segment inverted vertical>
+      <Container>
+        <Grid.Row>
+          <Grid.Column width={12}>
+            <p id='attribution'>
+              designed and developed by
+              {' '}
+              <a href='https://zachhardesty.com'>Zach Hardesty</a>
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Container>
+    </Segment>
+  </>
+)
+
+export default Footer
