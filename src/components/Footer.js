a94eea3b168ce4f61bf91f32cc854db0905e62c1
zach797a <zachhardesty7@gmail.com>
2018-09-18 17:17:16 -0500

    remove unused footer templating

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,56 +11,20 @@ import SocialMediaIcons from './SocialMediaIcons'
 import './Footer.scss'
 
 const Footer = () => (
-  <>
-    <Segment inverted vertical id='footer'>
-      <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
-              <List link inverted>
-                <List.Item as='a'>Sitemap</List.Item>
-                <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as='h4' inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
-    <Segment inverted vertical id='bottom-bar'>
-      <Container>
-        <Grid colums={2}>
-          <Grid.Column id='attribution' width={8}>
+  <Segment inverted vertical id='bottom-bar'>
+    <Container>
+      <Grid colums={2}>
+        <Grid.Column id='attribution' width={8}>
             copyright &#169; 2018 connectdoor | designed and developed by
-            {' '}
-            <a href='https://zachhardesty.com'>Zach Hardesty</a>
-          </Grid.Column>
-          <Grid.Column width={8} floated='right' textAlign='right'>
-            <SocialMediaIcons inverted />
-          </Grid.Column>
-        </Grid>
-      </Container>
-    </Segment>
-  </>
+          {' '}
+          <a href='https://zachhardesty.com'>Zach Hardesty</a>
+        </Grid.Column>
+        <Grid.Column width={8} id='bottom-bar-icons' floated='right' textAlign='right'>
+          <SocialMediaIcons inverted />
+        </Grid.Column>
+      </Grid>
+    </Container>
+  </Segment>
 )
 
 export default Footer
