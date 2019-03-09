9f1d89c5eaba554633e979b63c78378c05dbfe77
Zach Hardesty <zachhardesty7@gmail.com>
2019-03-08 18:12:12 -0600

    add in css bundle cleaning

--- babel.config.js
+++ babel.config.js
@@ -25,7 +25,7 @@ module.exports = {
   ],
   plugins: [
     // TODO: breaks when using npm linked version of semantic-styled-ui
-    // 'babel-plugin-transform-semantic-ui-react-style-imports',
+    'babel-plugin-transform-semantic-ui-react-style-imports',
     [
       'transform-react-remove-prop-types', {
         removeImport: true
