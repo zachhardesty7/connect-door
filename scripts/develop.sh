5ed6cd60c5b2a9b9824a35d2aed9a5b99d34ec67
Zach Hardesty <hello@zachhardesty.com>
2020-05-22 17:43:15 -0500

    add build scripts for SSUI

---
+++ scripts/develop.sh
@@ -0,0 +1,22 @@
+scripts/use-local-library.sh $1
+
+printf "$(tput bold)building library in watch mode (yarn dev)$(tput sgr0)\n\n"
+
+path=$(mktemp)
+
+cd $1
+
+# start watcher and send output to stdout and temp file
+(yarn watch 2>&1 | tee $path) &
+# WATCHER=$!
+
+# continuously read temp file while program is running
+tail -F $path | while read line; do
+  res=$(printf "$line" | grep -E "created .* in .*")
+  if [ -n "$res" ]; then
+    printf "\n$(tput bold)library watcher started successfully, launching gatsby$(tput sgr0)\n\n"
+    cd -
+    gatsby develop
+    exit
+  fi
+done
